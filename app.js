cd079c9be0f0e2502e9931d71b13fce8da1978e5
TheWhiteWolf398 <abhishekmukherjee398@gmail.com>
2023-10-02 20:47:48 +0530

    Add bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -24,8 +24,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret : process.env.SECRET , encryptedFields: ["password"]});
-
 const User = new mongoose.model("User", userSchema);
 
 
@@ -44,7 +42,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req,res){
   const newUser= new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
 });
 
 newUser.save().then(()=>{
@@ -56,7 +54,7 @@ newUser.save().then(()=>{
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
   
   
 User.findOne({email:username})
@@ -75,7 +73,6 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(port);
      
 app.listen(port, function(){
   console.log("Server has started successfully.");
